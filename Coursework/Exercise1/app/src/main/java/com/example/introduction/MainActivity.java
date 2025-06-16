84598550e6f4224c6e47289fa8d3b6a22f9246b9
liasouris <lia.souris@student.lut.fi>
2025-06-16 13:02:47 +0300

    Add Exercise1 Android project to Coursework folder

---
+++ Coursework/Exercise1/app/src/main/java/com/example/introduction/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.introduction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText firstNumberEditText;
+    private EditText secondNumberEditText;
+    private TextView resultTextView;
+    private Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addButton);
+        firstNumberEditText = findViewById(R.id.firstNumberEditText);
+        secondNumberEditText = findViewById(R.id.secondNumberEditText);
+        resultTextView = findViewById(R.id.resultTextView);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(firstNumberEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumberEditText.getText().toString());
+                int sum = num1 + num2;
+                resultTextView.setText(String.valueOf(sum));
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
